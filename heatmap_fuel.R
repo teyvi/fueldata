efd1d654d5a98f61dc16ea39552fa83f9c9d5cc1
teyvi <teyviangela@yahoo.com>
2022-01-21 21:48:27 +0000

    add R

---
+++ heatmap_fuel.R
@@ -0,0 +1,16 @@
+install.packages("plyr")
+install.packages("lubridate")
+install.packages("ggplot2")
+install.packages("dplyr")
+
+# Install the relevant libraries - do this one time 
+install.packages("plyr")
+install.packages("lubridate")
+install.packages("ggplot2")
+install.packages("dplyr")
+
+# Load the relevant libraries - do this every time
+library(plyr)
+library(lubridate)
+library(ggplot2)
+library(dplyr)
